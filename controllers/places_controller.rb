8a40b7b9a5a8a234870e62df31c842273a8e0bee
Tech-A34 <Tech-A34@Tech-A34s-MacBook-Air.local>
2018-06-18 19:14:07 +0100

    filled in show new edit and form

--- controllers/places_controller.rb
+++ controllers/places_controller.rb
@@ -16,6 +16,66 @@ class PlacesController < Sinatra::Base
     erb :'places/index'
   end
 
+  # New
+  get '/places/new' do
+    @place = Place.new
 
+    erb :'places/new'
+  end
+
+  # Show
+  get '/places/:id' do
+
+    id = params[:id]
+
+    @place = Place.find id
+
+    erb :'places/show'
+  end
+
+  # Create
+  post "/places/" do
+    place = Place.new
+
+    place.country_name = params[:country_name]
+    place.currency = params[:currency]
+
+    place.save
+
+    redirect '/places'
+  end
+
+  # Update
+  put '/places/:id' do
+
+    id = params[:id]
+    place = Place.find id
+
+    place.country_name = params[:country_name]
+    place.currency = params[:currency]
+
+    place.save
+
+    redirect '/places'
+  end
+
+  # Destroy
+  delete '/places/:id'  do
+    id = params[:id].to_i
+
+    Place.destroy id
+
+    redirect '/places'
+  end
+
+  # Edit
+  get '/places/:id/edit'  do
+
+    id = params[:id]
+
+    @place = Place.find id
+
+    erb :'places/edit'
+  end
 
 end
